f462e24ee36d0d11b5ba8edfa2431c8ab9558be7
Shaanshimpi <shaanshimpi@gmail.com>
2025-07-02 14:26:09 +0530

    initial stage

--- src/components/Branding/Logo.tsx
+++ src/components/Branding/Logo.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { CustomComponent } from 'payload'
 
-const Logo: CustomComponent = (props) => {
+const Logo: CustomComponent = () => {
   return <div style={{ fontSize: '4.2rem', fontWeight: 'bold' }}>CodeHub Admin</div>
 }
 
