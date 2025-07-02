a0242711198b592d5648e7de70a8f8ec4da9a3be
Shaanshimpi <shaanshimpi@gmail.com>
2025-07-02 14:31:14 +0530

    initial stage

--- src/components/Branding/Icon.tsx
+++ src/components/Branding/Icon.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { CustomComponent } from 'payload'
 
-const Icon: CustomComponent = () => {
+const Icon = () => {
   return <div style={{ fontWeight: 'bold', fontSize: '1.5rem' }}>🔥</div> // or your SVG/icon
 }
 
--- src/components/Branding/Logo.tsx
+++ src/components/Branding/Logo.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { CustomComponent } from 'payload'
 
-const Logo: CustomComponent = () => {
+const Logo = () => {
   return <div style={{ fontSize: '4.2rem', fontWeight: 'bold' }}>CodeHub Admin</div>
 }
 
