48d294a7c5f27891ff41697e5d134174a7908922
Shaanshimpi <shaanshimpi@gmail.com>
2025-07-05 11:02:35 +0530

    allowed access

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -2,6 +2,13 @@ import type { CollectionConfig } from 'payload'
 
 export const Users: CollectionConfig = {
   slug: 'users',
+  access: {
+    read: ({ req }) => {
+      const allowedHosts = ['localhost:3000', 'localhost:3001', 'codehubindia.in'] // your frontend domains
+      const host = req.headers.get('x-forwarded-host') || req.headers.get('host') || ''
+      return allowedHosts.includes(host)
+    },
+  },
   admin: {
     useAsTitle: 'email',
   },
